6f0db47d28e31c2c4e23ac2495e5c5f733d30311
SolomonV01 <113135388+SolomonV01@users.noreply.github.com>
2025-05-01 17:37:35 -0500

    Javascript quotes update1

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const faders = document.querySelectorAll('.stat-1');
 
 const appearOptions = {
-  threshold: 0.1,
+  threshold: 0.3,
   rootMargin: '0px 0px -50px 0px'
 };
 
@@ -19,3 +19,48 @@ faders.forEach(fader => {
   });
 
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const faders1 = document.querySelectorAll('.stat-2');
+    
+    const appearOptions = {
+      threshold: 0.3,
+      rootMargin: '0px 0px -100px 0px'
+    };
+    
+    const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) return;
+        entry.target.classList.add('visible1');
+        observer.unobserve(entry.target);
+      });
+    }, appearOptions);
+    
+    faders1.forEach(fader1 => {
+        appearOnScroll.observe(fader1);
+      });
+    
+    });
+    
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const faders2 = document.querySelectorAll('.stat-2');
+        
+        const appearOptions = {
+          threshold: 0.3,
+          rootMargin: '0px 0px -100px 0px'
+        };
+        
+        const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+          entries.forEach(entry => {
+            if (!entry.isIntersecting) return;
+            entry.target.classList.add('visible2');
+            observer.unobserve(entry.target);
+          });
+        }, appearOptions);
+        
+        faders2.forEach(fader2 => {
+            appearOnScroll.observe(fader2);
+          });
+        
+        });
